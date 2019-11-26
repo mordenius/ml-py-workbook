8250d2dd38148d821ce8138ce817206950b6b9e4
mordenius <mordenius@gmail.com>
2019-11-26 17:49:48 +0200

    feature(project_kaggle_courses/intro_to_machine_learning) DecisionTreeRegressor vs RandomForestRegressor

---
+++ project_kaggle_courses/intro_to_machine_learning/intro.py
@@ -0,0 +1,52 @@
+import os
+
+import pandas as pd
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+iowa_file_path = './datasets/train.csv'
+
+home_data = pd.read_csv(os.path.join(current_dir, iowa_file_path), sep=",")
+
+y = home_data.SalePrice
+
+features = ['LotArea', 'YearBuilt', '1stFlrSF',
+            '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print(
+    "Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# Using best value for max_leaf_nodes
+iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print(
+    "Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1, n_estimators=100)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
