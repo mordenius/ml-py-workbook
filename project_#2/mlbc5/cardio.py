70dfc153ef11fff061afec8b4193f67446975f23
Mordenius Norrogem <mordenius@gmail.com>
2019-11-26 23:18:36 +0200

    feature(project_#2/mlbc5) data visualisation

--- project_#2/mlbc5/cardio.py
+++ project_#2/mlbc5/cardio.py
@@ -32,3 +32,29 @@ mean_cardio_by_gender_and_chole = cardio_data.groupby(['gender', 'cholesterol'])
 cardio_data['smoke_and_alco'] = cardio_data[['smoke', 'alco']].apply(lambda x: x[0] * x[1], axis=1)
 mean_cardio_by_smoke_and_alco = cardio_data.groupby(['smoke_and_alco'])['cardio'].aggregate('mean')
 # print(mean_cardio_by_smoke_and_alco)
+
+# Draw plot for `height` and `weight` 
+plt.figure(figsize=(12,8))
+plt.scatter(cardio_data.weight, cardio_data.height, s=9, c=cardio_data.cardio, cmap='seismic')
+plt.colorbar()
+plt.xlabel('weight')
+plt.ylabel('height')
+# plt.show()
+
+# Draw plot for `ap_lo` and `ap_hi` 
+plt.figure(figsize=(12,8))
+plt.scatter(cardio_data.ap_lo, cardio_data.ap_hi, s=9, c=cardio_data.cardio, cmap='seismic')
+plt.colorbar()
+plt.xlabel('ap_lo')
+plt.ylabel('ap_hi')
+# plt.show()
+
+f, axes = plt.subplots(6, 1)
+
+sns.boxplot(cardio_data.weight, ax=axes[0])
+sns.distplot(cardio_data.weight, ax=axes[1])
+sns.boxplot(cardio_data.height, ax=axes[2])
+sns.distplot(cardio_data.height, ax=axes[3])
+sns.boxplot(cardio_data.age, ax=axes[4])
+sns.distplot(cardio_data.age, ax=axes[5])
+plt.show()
