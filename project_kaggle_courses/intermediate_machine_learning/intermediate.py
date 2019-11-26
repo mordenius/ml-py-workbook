358ae6561f7d30fe413d138e5dd8f643579e590e
mordenius <mordenius@gmail.com>
2019-11-26 17:58:11 +0200

    feature(project_kaggle_courses/intermediate_machine_learning) getting data

---
+++ project_kaggle_courses/intermediate_machine_learning/intermediate.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+iowa_train_file_path = '../intro_to_machine_learning/datasets/train.csv'
+iowa_test_file_path = '../intro_to_machine_learning/datasets/test.csv'
+
+
+# Read the data
+X_full = pd.read_csv(os.path.join(current_dir, iowa_train_file_path), index_col='Id')
+X_test_full = pd.read_csv(os.path.join(current_dir, iowa_test_file_path), index_col='Id')
+
+# Obtain target and predictors
+y = X_full.SalePrice
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = X_full[features].copy()
+X_test = X_test_full[features].copy()
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
