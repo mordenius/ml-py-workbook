20571b155188e4eb92f400a5aa1905eadfd7a337
Mordenius Norrogem <mordenius@gmail.com>
2019-11-29 22:10:50 +0200

    chore(project_#1/05_reinforcement_learning/q_learning_numpy_only) prepare parameters

---
+++ project_#1/05_reinforcement_learning/q_learning_numpy_only.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+# Initialize parameters
+gamma = 0.75  # Discount factor
+alpha = 0.9  # Learning rate
+
+# Define the states
+location_to_state = {
+    'L1': 0,
+    'L2': 1,
+    'L3': 2,
+    'L4': 3,
+    'L5': 4,
+    'L6': 5,
+    'L7': 6,
+    'L8': 7,
+    'L9': 8
+}
+
+# Define the actions
+actions = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+# Defina the rewards
+rewards = np.array([
+    [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0]
+])
+
+# Maps indices to Location
+state_to_location = dict((state, location) for location, state in location_to_state.items())
