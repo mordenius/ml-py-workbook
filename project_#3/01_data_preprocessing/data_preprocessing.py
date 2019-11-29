9b11796432d61294c96345cb6d4ee4dc58f4589d
Mordenius Norrogem <mordenius@gmail.com>
2019-11-30 01:41:12 +0200

    feature(project_#3/01_data_preprocessing) standardisation scalling via sklearn

--- project_#3/01_data_preprocessing/data_preprocessing.py
+++ project_#3/01_data_preprocessing/data_preprocessing.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -40,3 +41,10 @@ if __name__ == "__main__":
 
     X_train, X_test, y_train,  y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
+
+    sc_X = StandardScaler()
+    X_train = sc_X.fit_transform(X_train)
+    X_test = sc_X.transform(X_test)
+
+    sc_y = StandardScaler()
+    y_train = sc_y.fit_transform(y_train.reshape(-1, 1))
