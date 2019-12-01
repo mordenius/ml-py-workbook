43f7f34d05a3ee8f32cc50db829d88eb147ee536
Mordenius Norrogem <mordenius@gmail.com>
2019-12-01 22:07:22 +0200

    chore(project_#5/01_numpy) accessing

--- project_#5/01_numpy.py
+++ project_#5/01_numpy.py
@@ -30,6 +30,43 @@ def basics():
     print('Count of Values in array \'b\' is {}'.format(b.size))
 
 
-if __name__ == '__main__':
-    basics()
+def accessing():
+    a = np.array([[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14]], dtype='int8')
 
+    # Get a specific element
+    # [row, column]
+    print('Item on second row and fifth column is {}'.format(a[1, 4]))
+
+    # Get a specific row
+    print('First row is {}'.format(a[0, :]))
+
+    # Get a specific column
+    print('Third column is {}'.format(a[:, 2]))
+
+    # Slice with sized step
+    print('Even from second and one before end from first row is {}'.format(a[0, 1:-1:2]))
+
+    # Change by index
+    print('Before value is {}'.format(a[0, :]))
+    a[0, 3] = 25
+    print('After value is {}'.format(a[0, :]))
+
+    # Change by index with array
+    print('Before value is {}'.format(a[:, 4]))
+    a[:, 4] = [96, 73]
+    print('After value is {}'.format(a[:, 4]))
+
+    # 3d
+    b = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+    # Get specific element
+    print('--> {}'.format(b[0, 1, 1]))
+
+    # replace
+    b[:, 1, :] = [[7, 7, 7], [8, 8, 8]]
+    print('--> {}'.format(b))
+
+
+if __name__ == '__main__':
+    # basics()
+    accessing()
