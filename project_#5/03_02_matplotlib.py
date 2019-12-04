cda47c7bd35bf2fe5719c4d8d27934f0c2681aa8
Mordenius Norrogem <mordenius@gmail.com>
2019-12-04 22:21:15 +0200

    chore(project_#5/03_matplotlib) subplots

--- project_#5/03_02_matplotlib.py
+++ project_#5/03_02_matplotlib.py
@@ -1,13 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from matplotlib import style
+# from matplotlib import style
 import numpy as np
 
-style.use('fivethirtyeight')
+# style.use('fivethirtyeight')
 
 fig = plt.figure()
-axl = fig.add_subplot(1, 1, 1)
+# axl = fig.add_subplot(2, 2, 1)
+# axl = fig.add_subplot(2, 2, 2)
+# axl = fig.add_subplot(2, 1, 2)
 
+axl = plt.subplot2grid((6,1), (0,0), rowspan=1, colspan=1)
+ax2 = plt.subplot2grid((6,1), (1,0), rowspan=4, colspan=1)
+ax3 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
 
 def animate(i):
     xs = np.random.random(5)
@@ -16,6 +21,10 @@ def animate(i):
     axl.clear()
     axl.plot(xs, ys)
 
+    ax2.plot(ys, xs)
+
+    ax3.clear()
+    ax3.plot(ys, xs)
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
