38a655fbd0e6bac1314a4306e5b0ed0743ed0339
Mordenius Norrogem <mordenius@gmail.com>
2019-12-05 22:19:19 +0100

    feature(project_#4/my_math) vectors

--- project_#4/my_math.py
+++ project_#4/my_math.py
@@ -34,6 +34,60 @@ def get_adjusted_r_of_squares(sample, predicted, count_of_regressors):
     return 1 - (1 - r_of_squares) / ((len(sample) - 1) / (len(sample) - count_of_regressors - 1))
 
 
+def vector_add(v, w):
+    '''Vectors addition by element'''
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_substract(v, w):
+    '''Vectors subtract by element'''
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_sum(vectors):
+    '''Vectors addition by element'''
+    # recuce(vector_add, vectors)
+    result = vectors[0]
+    for vector in vectors[1:]:
+        result = vector_add(result, vector)
+    return result
+
+
+def scalar_multiply(scalar, vector):
+    return [scalar * v_i for v_i in vector]
+
+
+def vector_mean(vectors):
+    n = len(vectors)
+    return scalar_multiply(1 / n, vector_sum(vectors))
+
+
+def dot(v, w):
+    '''v_i * w_i + ... + v_n * w_n'''
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v):
+    '''v_i * v_i + ... + v_n * v_n'''
+    return dot(v, v)
+
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares())
+
+
+def squared_distance(v, w):
+    '''Squared distance between two vectors
+
+    (v_i - w_i) ** 2 + ... + (v_n * w_n) ** 2'''
+    return sum_of_squares(vector_substract(v, w))
+
+
+def distance(v, w):
+    '''Distance between two vectors'''
+    return magnitude(vector_substract(v, w))
+
+
 if __name__ == '__main__':
     test_sample = [1, 2, 2, 3, 4, 4, 5]
     test_predicted = [1, 2, 2, 3, 4, 3, 5]
