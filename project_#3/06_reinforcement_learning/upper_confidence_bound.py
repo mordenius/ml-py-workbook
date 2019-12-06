fe1f207c143cb70004c0cfae476eb02a0e161b25
Mordenius Norrogem <mordenius@gmail.com>
2019-12-06 03:47:25 +0100

    refactor(project_#3/06_reinforcement_learning/upper_confidence_bound) template for selections process

--- project_#3/06_reinforcement_learning/upper_confidence_bound.py
+++ project_#3/06_reinforcement_learning/upper_confidence_bound.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 import datasets.dataset_provider as data_provider
 import matplotlib.pyplot as plt
@@ -8,22 +9,44 @@ dataset = data_provider.get_ads_ctr_optimisation()
 fig = plt.figure()
 plt.subplots_adjust(left=0.08, bottom=0.16, right=0.95, wspace=0.2, hspace=0)
 
-# Implementing Random Selection
-import random
+class Selections:
+    def __init__(self, algorithm):
+        self.N = 10000
+        self.d = 10
+        self.algorithm = algorithm
+        self.ads_selected = []
+        self.total_reward = 0
 
-N = 10000
-d = 10
-ads_selected = []
-total_reward = 0
-for n in range(0, N):
-    ad = random.randrange(d)
-    ads_selected.append(ad)
+    def start(self):
+        for n in range(0, self.N):
+            self.algorithm(self, n)
+
+
+def random_algorithm(selections, n):
+    ad = random.randrange(selections.d)
+    selections.ads_selected.append(ad)
     reward = dataset.values[n, ad]
-    total_reward = total_reward + reward
+    selections.total_reward = selections.total_reward + reward
+
+
+# Implementing Random Selection
+# N = 10000
+# d = 10
+# ads_selected = []
+# total_reward = 0
+# for n in range(0, N):
+#     ad = random.randrange(d)
+#     ads_selected.append(ad)
+#     reward = dataset.values[n, ad]
+#     total_reward = total_reward + reward
+
+
+random_selection = Selections(random_algorithm)
+random_selection.start()
 
 # Visualising the results
 left = plt.subplot2grid((1, 2), (0, 0))
-left.hist(ads_selected)
+left.hist(random_selection.ads_selected)
 plt.title('Histogram of ads selections (Random)')
 plt.xlabel('Ads')
 plt.ylabel('Number of times each ad was selected')
@@ -61,3 +84,4 @@ plt.title('Histogram of ads selections (UCB)')
 plt.xlabel('Ads')
 plt.ylabel('Number of times each ad was selected')
 plt.show()
+
