45512ef28d643163e1e9caeded3e5778b7946f75
Mordenius Norrogem <mordenius@gmail.com>
2019-12-06 03:27:31 +0100

    chore(project_#3/06_reinforcement_learning) random selection demo

---
+++ project_#3/06_reinforcement_learning/upper_confidence_bound.py
@@ -0,0 +1,23 @@
+import datasets.dataset_provider as data_provider
+import matplotlib.pyplot as plt
+
+dataset = data_provider.get_ads_ctr_optimisation()
+
+# Implementing Random Selection
+import random
+N = 10000
+d = 10
+ads_selected = []
+total_reward = 0
+for n in range(0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = total_reward + reward
+
+# Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
