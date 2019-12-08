aaf5dbeb02000c0f352ea275b9340c8642ba2f62
Mordenius Norrogem <mordenius@gmail.com>
2019-12-08 02:43:00 +0200

    chore(project_#4) __init__

---
+++ project_#4/__init__.py
@@ -0,0 +1,5 @@
+from .linear_algebra import vectors, matrix
+
+from .statistics import measures_of_central_tendency
+from .statistics import measures_of_variability
+from .statistics import correlation
