346819415c9aa8961d15370d1907adfd16da0c79
Mordenius Norrogem <mordenius@gmail.com>
2019-12-09 03:48:15 +0200

    feature(project_#4/combinatorics) permutations without repetition

--- project_#4/combinatorics/combinatorics.py
+++ project_#4/combinatorics/combinatorics.py
@@ -3,3 +3,7 @@ import math
 
 def combinations_without_repetition(n, k):
     return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+
+def permutations_without_repetition(n):
+    return math.factorial(n)
