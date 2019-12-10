65793e356c6fdc90af4bafb46a79170c5e93cfb8
Mordenius Norrogem <mordenius@gmail.com>
2019-12-10 02:49:27 +0200

    feature(project_#04/07_natural_language_processing) predict via naïve bayes

--- project_#3/07_natural_language_processing/natural_language_processing.py
+++ project_#3/07_natural_language_processing/natural_language_processing.py
@@ -3,6 +3,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 import datasets.dataset_provider as data_provider
 
@@ -24,3 +27,14 @@ for rev in dataset['Review']:
 cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting a new result with Regression
+predictions = classifier.predict(X_test)
+cm = confusion_matrix(y_test, predictions)
+print(cm)
