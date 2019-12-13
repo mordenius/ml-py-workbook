323e353b42550a34bf86a22e490f5d0ef8a7ff94
Mordenius Norrogem <mordenius@gmail.com>
2019-12-13 23:41:02 +0200

    chore(project_#3/08_deep_learning/artificial_neural_network) encoding categorical data

--- project_#3/08_deep_learning/artificial_neural_network.py
+++ project_#3/08_deep_learning/artificial_neural_network.py
@@ -1,4 +1,8 @@
 import datasets.dataset_provider as data_provider
+import numpy as np
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 
 def load_dataset():
@@ -11,3 +15,8 @@ if __name__ == '__main__':
 
     X = dataset.iloc[:, 3:13].values
     y = dataset.iloc[:, 13].values
+
+    # Encoding categorical data
+    ct = ColumnTransformer(
+        [('encoder', OneHotEncoder(), [1, 2])], remainder='passthrough')
+    X = np.array(ct.fit_transform(X), dtype=np.float)
