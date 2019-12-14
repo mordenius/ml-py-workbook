5c884d34658a45880e320e8b3562c4b8d97741b7
Mordenius Norrogem <mordenius@gmail.com>
2019-12-14 03:54:40 +0200

    feature(project_#3/09_dimensionality_reduction) principal component analysis

--- project_#3/09_dimensionality_reduction/principal_component_analysis.py
+++ project_#3/09_dimensionality_reduction/principal_component_analysis.py
@@ -9,6 +9,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+from sklearn.decomposition import PCA
 
 def load_dataset():
     """ Reading Data """
@@ -48,6 +49,12 @@ if __name__ == '__main__':
     sc_X = StandardScaler()
     X_train = sc_X.fit_transform(X_train)
     X_test = sc_X.fit_transform(X_test)
+
+    # Applying PCA
+    pca = PCA(n_components=2)
+    X_train = pca.fit_transform(X_train)
+    X_test = pca.fit_transform(X_test)
+    explained_variance = pca.explained_variance_ratio_
 
     classifier = LogisticRegression(random_state=0, solver='lbfgs', multi_class='auto')
     classifier.fit(X_train, y_train)
