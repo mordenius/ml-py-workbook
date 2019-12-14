d9f1fed5e597a00cad072195370e717781f34ff8
Mordenius Norrogem <mordenius@gmail.com>
2019-12-14 03:28:18 +0200

    chore(project_#3/08_deep_learning/convolutional_neural_network) fit to the dataset

--- project_#3/08_deep_learning/convolutional_neural_network.py
+++ project_#3/08_deep_learning/convolutional_neural_network.py
@@ -3,6 +3,7 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras_preprocessing.image import ImageDataGenerator
 
 # Initializing the Convolutional neural network (CNN)
 classifier = Sequential()
@@ -22,3 +23,31 @@ classifier.add(Dense(output_dim=1, activation="sigmoid"))
 
 # Compiling the CNN
 classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# Fitting CNN to the Training set
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    'datasets/cnn/training_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    'datasets/cnn/test_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
+
+classifier.fit_generator(
+    train_generator,
+    steps_per_epoch=8000,
+    epochs=25,
+    validation_data=validation_generator,
+    validation_steps=200)
