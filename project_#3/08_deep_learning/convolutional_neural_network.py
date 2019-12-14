7cd3914d22d8d3b75861d06d11bcc5371d79f814
Mordenius Norrogem <mordenius@gmail.com>
2019-12-14 03:13:45 +0200

    chore(project_#3/08_deep_learning/convolutional_neural_network) add second (pooling) layer

--- project_#3/08_deep_learning/convolutional_neural_network.py
+++ project_#3/08_deep_learning/convolutional_neural_network.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Convolution2D
-from keras.layers import MaxPooling3D
+from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
@@ -9,3 +9,6 @@ classifier = Sequential()
 
 # Convolution layer
 classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation="relu"))
+
+# Pooling
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
